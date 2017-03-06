acbe3259c32586337a79491624c19859c40567a6
Carlos Silva <csilva2810@gmail.com>
2017-03-06 17:23:33 -0300

    Add Hero, Delete Hero and styles

--- src/app/hero.service.ts
+++ src/app/hero.service.ts
@@ -50,5 +50,27 @@ export class HeroService {
       .catch(this.handleError);
 
   }
+
+  create(name: string): Promise<hero> {
+
+    return this.http
+      .post(this.heroesUrl, JSON.stringify({name: name}), {headers: this.headers})
+      .toPromise()
+      .then(response => response.json().data)
+      .catch(this.handleError);
+
+  }
+
+  delete(id: number): Promise<void> {
+
+    const url = `${this.heroesUrl}/${id}`;
+
+    return this.http
+      .delete(url, {headers: this.headers})
+      .toPromise()
+      .then(() => null)
+      .catch(this.handleError);
+
+  }
   
 }
