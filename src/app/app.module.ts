5326aae1ede1604338841c9f20eb7ea6ee889f97
Carlos Silva <csilva2810@gmail.com>
2017-03-30 19:05:42 -0300

    preparing to start coding

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,9 +1,6 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
-import { InMemoryWebApiModule } from 'angular-in-memory-web-api';
-import { InMemoryDataService } from './in-memory-data.service';
-
 import { AppComponent } from './app.component';
 
 @NgModule({
